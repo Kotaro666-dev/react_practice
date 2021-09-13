831181f76959b3f3d8cc925dd4f5530c7c705dc4
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-14 08:00:58 +0900

    Update: Add conditional case to expenses content

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -16,21 +16,27 @@ function Expenses(props) {
 		return item.date.getFullYear().toString() === expensesFilterDate;
 	});
 
+	let expensesContent;
+
+	if (filteredExpenses.length === 0) {
+		expensesContent = <p>No expenses found.</p>;
+	} else {
+		expensesContent = filteredExpenses
+			.map(item =>
+				<ExpenseItem
+					key={item.id}
+					title={item.title}
+					amount={item.amount}
+					date={item.date}
+				/>
+		);
+	}
+
 	return (
 		<div>
 			<Card className="expenses" >
 				<ExpensesFilter selectedYear={expensesFilterDate}  onSaveExpensesFilterDate={onSaveExpensesFilterDateHandler} />
-				{
-					filteredExpenses
-						.map(item =>
-							<ExpenseItem
-								key={item.id}
-								title={item.title}
-								amount={item.amount}
-								date={item.date}
-							/>
-						)
-				}
+				{ expensesContent }
 			</Card>
 		</div>
 	);
