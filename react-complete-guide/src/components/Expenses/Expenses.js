d8a46ec45b49f635d15811a88dc56b825a08868c
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-14 08:13:41 +0900

    Update: Create new file for rendering expensesList

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,4 +1,6 @@
-import ExpenseItem from "./ExpenseItem";
+import React from "react";
+
+import ExpensesList from "./ExpensesList";
 import Card from '../UI/Card';
 import './Expenses.css'
 import ExpensesFilter from "./ExpensesFilter";
@@ -16,27 +18,12 @@ function Expenses(props) {
 		return item.date.getFullYear().toString() === expensesFilterDate;
 	});
 
-	let expensesContent;
-
-	if (filteredExpenses.length === 0) {
-		expensesContent = <p>No expenses found.</p>;
-	} else {
-		expensesContent = filteredExpenses
-			.map(item =>
-				<ExpenseItem
-					key={item.id}
-					title={item.title}
-					amount={item.amount}
-					date={item.date}
-				/>
-		);
-	}
 
 	return (
 		<div>
 			<Card className="expenses" >
 				<ExpensesFilter selectedYear={expensesFilterDate}  onSaveExpensesFilterDate={onSaveExpensesFilterDateHandler} />
-				{ expensesContent }
+				<ExpensesList items={filteredExpenses}/>
 			</Card>
 		</div>
 	);
