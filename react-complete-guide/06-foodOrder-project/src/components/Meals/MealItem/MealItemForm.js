12830c48866a03583e02e33854775d07e939831b
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-19 16:53:45 +0900

    Update: Add item form

--- react-complete-guide/06-foodOrder-project/src/components/Meals/MealItem/MealItemForm.js
+++ react-complete-guide/06-foodOrder-project/src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,14 @@
 import React from "react";
 
 import classes from './MealItemForm.module.css';
+import Input from "../../UI/Input";
 
 const MealItemForm = () => {
 	return (
-		<div>
-			<form className={classes.form}>
-				<span>Amount</span>
-				<input type="text" />
-			</form>
+		<form className={classes.form}>
+			<Input label="Amount" input={{id: 'amount', type: 'number', min: '1', max: '10', step: '1', defaultValue: '1' }} />
 			<button className={classes.button}>+ Add</button>
-		</div>
+		</form>
 	);
 };
 
