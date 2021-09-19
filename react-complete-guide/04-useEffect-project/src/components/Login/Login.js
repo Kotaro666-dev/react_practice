ccf81dd85897964674bb3eac14f7144b54c13b6a
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-19 09:46:26 +0900

    Update: Add cleanup function

--- react-complete-guide/04-useEffect-project/src/components/Login/Login.js
+++ react-complete-guide/04-useEffect-project/src/components/Login/Login.js
@@ -12,9 +12,19 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
 	useEffect(() => {
-		setFormIsValid(
-			enteredEmail.includes('@') && enteredPassword.trim().length > 6
-		);
+		const identifier = setTimeout(() => {
+			// console.log('Checking form validity.');
+			setFormIsValid(
+				enteredEmail.includes('@') && enteredPassword.trim().length > 6
+			);
+		}, 500);
+		clearTimeout();
+
+		// Cleanup Function
+		return () => {
+			// console.log('Clean up');
+			clearTimeout(identifier);
+		};
 	}, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
