618fa21b67a9caed7eb749faad7ea6457b473b90
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-19 12:51:44 +0900

    Update: Refactor context

--- react-complete-guide/05-usereducer-starting-project/src/components/Home/Home.js
+++ react-complete-guide/05-usereducer-starting-project/src/components/Home/Home.js
@@ -1,11 +1,16 @@
+import React, { useContext } from 'react';
+import AuthContext from '../store/auth-context';
+
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
 const Home = (props) => {
+	const context = useContext(AuthContext);
+
 	return (
 		<Card className={classes.home}>
 			<h1>Welcome back!</h1>
-			<button onClick={props.onLogout}>Logout</button>
+			<button onClick={context.onLogout}>Logout</button>
 		</Card>
 	);
 };
--- react-complete-guide/05-usereducer-starting-project/src/components/Login/Login.js
+++ react-complete-guide/05-usereducer-starting-project/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../store/auth-context';
 
 const emailReducer = (prevState, action) => {
 	if (action.type === 'USER_INPUT') {
@@ -30,6 +31,7 @@ const Login = (props) => {
 //   const [enteredPassword, setEnteredPassword] = useState('');
 //   const [passwordIsValid, setPasswordIsValid] = useState();
 	const [formIsValid, setFormIsValid] = useState(false);
+	const context = useContext(AuthContext);
 
 
 	const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: null});
@@ -86,7 +88,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    context.onLogin(emailState.value, passwordState.value);
   };
 
   return (
