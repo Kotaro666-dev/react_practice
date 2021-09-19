53a30d792e37395b991966112f2eedd2019deb98
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-19 09:14:28 +0900

    Update: Use effect without dependencies

--- react-complete-guide/04-useEffect-project/src/App.js
+++ react-complete-guide/04-useEffect-project/src/App.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+const TRUE = '1';
+const FALSE = '0';
+const isLoggedInKey = 'isLoggedIn';
+
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+	const loginHandler = (email, password) => {
+		// We should of course check email and password
+		// But it's just a dummy/ demo anyways
+		localStorage.setItem(isLoggedInKey, TRUE);
+		setIsLoggedIn(true);
+	};
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+	const logoutHandler = () => {
+		localStorage.setItem(isLoggedInKey, FALSE);
+		setIsLoggedIn(false);
+	};
+
+	useEffect(() => {
+		const storedUserLoggedInInformation = localStorage.getItem(isLoggedInKey);
+
+		if (storedUserLoggedInInformation === TRUE) {
+			setIsLoggedIn(true);
+		}
+	}, []);
 
   return (
     <React.Fragment>
