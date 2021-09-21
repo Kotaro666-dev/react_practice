e4e8f0bc9d81710f705061e778ad3b2d214dbbfd
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-21 23:28:49 +0900

    Update: Check how react dom reacts to state change

--- react-complete-guide/07-optimaization/src/App.js
+++ react-complete-guide/07-optimaization/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from './components/UI/Button/Button'
 import './App.css';
 
 function App() {
+	const [showParagraph, setShowParagraph] = useState(false);
+
+	const togglePragraphHandler = () => {
+		setShowParagraph((prevState) => !prevState);
+	}
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+	  {showParagraph && <p>This is new!</p>}
+	  <Button onClick={togglePragraphHandler}>Toggle paragraph!</Button>
     </div>
   );
 }
