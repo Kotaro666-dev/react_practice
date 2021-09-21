25083225d95e379f42bf9178437ab320d9d35809
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-21 22:49:58 +0900

    Update: Add animation to header cart button

--- react-complete-guide/06-foodOrder-project/src/components/Layout/HeaderCartButton.js
+++ react-complete-guide/06-foodOrder-project/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+	const [isButtonHighlighted, setIsButtonHighlighted] = useState(false);
 	const context = useContext(CartContext);
 
 	const numberOfCartItems = context.items.reduce((currenNumber, item) => {
 		return currenNumber + item.amount;
 	}, 0);
 
+	const buttonClasses = `${classes.button} ${isButtonHighlighted ? classes.bump : ''}`;
+
+	useEffect(() => {
+		if (context.totalAmount === 0) {
+			return;
+		}
+		setIsButtonHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setIsButtonHighlighted(false);
+		}, 300);
+
+		return (() => {
+			clearTimeout(timer);
+		});
+	}, [context.items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={buttonClasses} onClick={props.onClick}>
 			<span className={classes.icon}><CartIcon/></span>
 			<span>{props.children}</span>
 			<span className={classes.badge}>{numberOfCartItems}</span>
