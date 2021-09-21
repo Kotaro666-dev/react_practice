36daae05a7c81a488b075d06a84ed30ba8f54d6a
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-22 07:35:53 +0900

    Update: add react memo for optimization

--- react-complete-guide/07-optimaization/src/App.js
+++ react-complete-guide/07-optimaization/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Button from './components/UI/Button/Button'
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
 	const [showParagraph, setShowParagraph] = useState(false);
@@ -10,10 +11,12 @@ function App() {
 		setShowParagraph((prevState) => !prevState);
 	}
 
+	console.log('App running');
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-	  {showParagraph && <p>This is new!</p>}
+	  <DemoOutput show={false}/>
 	  <Button onClick={togglePragraphHandler}>Toggle paragraph!</Button>
     </div>
   );
