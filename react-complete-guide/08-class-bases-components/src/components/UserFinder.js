634e6088df20d555dc2c47e191a2bacdad42a318
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-22 22:06:59 +0900

    Update: Use context in class component

--- react-complete-guide/08-class-bases-components/src/components/UserFinder.js
+++ react-complete-guide/08-class-bases-components/src/components/UserFinder.js
@@ -2,18 +2,15 @@ import { Fragment, Component } from 'react';
 
 import classes from './UserFinder.module.css';
 import Users from './Users';
-
-const DUMMY_USERS = [
-	{ id: 'u1', name: 'Max' },
-	{ id: 'u2', name: 'Manuel' },
-	{ id: 'u3', name: 'Julie' },
-];
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
+	static contextType = UsersContext;
+
 	constructor() {
 		super();
 		this.state = {
-			filteredUsers: DUMMY_USERS,
+			filteredUsers: [],
 			searchTerm: '',
 		}
 	};
@@ -21,14 +18,14 @@ class UserFinder extends Component {
 	componentDidMount() {
 		// Send Http request...
 		this.setState({
-			filteredUsers: DUMMY_USERS,
+			filteredUsers: this.context.users,
 		});
 	}
 
 	componentDidUpdate(prevProps, prevState) {
 		if (prevState.searchTerm !== this.state.searchTerm) {
 			this.setState({
-				filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm)),
+				filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm)),
 			});
 		}
 	}
