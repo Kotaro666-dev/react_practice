2d0921d0a22bdf84b5ff93342945df898131c597
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-24 08:56:14 +0900

    Update: Add custom hooks for input validation

---
+++ react-complete-guide/12-working-with-forms-user-input/src/hooks/use-input.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const useInput = (validateValue) => {
+	const [enteredValue, setEnteredValue] = useState('');
+	const [isTouched, setIsTouched] = useState(false);
+
+	const isValueValid = validateValue(enteredValue);
+	const hasError = !isValueValid && isTouched;
+
+	const valueChangeHandler = (event) => {
+		setEnteredValue(event.target.value);
+	};
+
+	const inputBlurHandler = (event) => {
+		setIsTouched(true);
+	}
+
+	const reset = () => {
+		setEnteredValue('');
+		setIsTouched(false);
+	}
+
+	return {
+		value: enteredValue,
+		isValueValid: isValueValid,
+		hasError: hasError,
+		valueChangeHandler: valueChangeHandler,
+		InputBlurHandler: inputBlurHandler,
+		reset: reset,
+	};
+};
+
+export default useInput;
