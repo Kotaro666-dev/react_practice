e93046bc44b665def0ce88447ac117b486ca9a6a
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-23 12:08:00 +0900

    Update: Add useEffect and useCallback

--- react-complete-guide/09-http-request/src/App.js
+++ react-complete-guide/09-http-request/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -10,7 +10,7 @@ function App() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	const fetchMovieHandler = async () => {
+	const fetchMovieHandler = useCallback(async () => {
 		setIsLoading(true);
 		setError(null);
 		try {
@@ -32,7 +32,12 @@ function App() {
 			setError(error.message);
 		}
 		setIsLoading(false);
-	};
+	}, []);
+
+	useEffect(() => {
+		fetchMovieHandler();
+		return (() => {});
+	}, [fetchMovieHandler]);
 
 	// const fetchMovieHandler = async () => {
 	// 	fetch(url).then(response => {
