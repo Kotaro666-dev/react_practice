e5aac6f32fb117a200db62560f023b7fd7ccb223
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-23 12:30:43 +0900

    Update: Add post request

--- react-complete-guide/09-http-request/src/App.js
+++ react-complete-guide/09-http-request/src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
-const url = 'https://swapi.dev/api/films/';
+const url = 'firebase_url/movies.json';
 
 function App() {
 	const [movies, setMovies] = useState([]);
@@ -19,15 +20,18 @@ function App() {
 				throw new Error('Something went wrong!');
 			}
 			const data = await response.json();
-			const transformedMovies = data.results.map((movieData) => {
-				return {
-					id: movieData.episode_id,
-					title: movieData.title,
-					openingText: movieData.opening_crawl,
-					releaseDate: movieData.release_date,
-				};
-			});
-			setMovies(transformedMovies);
+
+			const loadedMovies = [];
+
+			for (const key in data) {
+				loadedMovies.push({
+					id: key,
+					title: data[key].title,
+					openingText: data[key].openingText,
+					releaseDate: data[key].releaseDate,
+				});
+			}
+			setMovies(loadedMovies);
 		} catch (error) {
 			setError(error.message);
 		}
@@ -56,6 +60,19 @@ function App() {
 	// 	});
 	// };
 
+	const addMovieHandler = async (movie) => {
+		const response = await fetch(url, {
+			method: 'POST',
+			body: JSON.stringify(movie),
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		});
+		const data = await response.json();
+		console.log(data);
+		// await fetchMovieHandler();
+	}
+
 	let content = <p>Found no movies.</p>;
 
 	if (movies.length > 0) {
@@ -72,6 +89,9 @@ function App() {
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler}/>
+			</section>
 			<section>
 			<button onClick={fetchMovieHandler}>Fetch Movies</button>
 			</section>
