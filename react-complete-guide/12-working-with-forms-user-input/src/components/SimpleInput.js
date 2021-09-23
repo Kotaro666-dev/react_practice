406c6983b81e6faf70918a5a417fccee11fe5bc5
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-24 08:58:19 +0900

    Update: Replace with custom hooks

--- react-complete-guide/12-working-with-forms-user-input/src/components/SimpleInput.js
+++ react-complete-guide/12-working-with-forms-user-input/src/components/SimpleInput.js
@@ -1,79 +1,55 @@
-import { useState } from 'react'
+import useInput from '../hooks/use-input';
 
 const SimpleInput = (props) => {
-	const [enteredName, setEnteredName] = useState('');
-	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+	const validateName = (value) => value.trim() !== '';
+	const validateEmail = (value) => value.trim() !== '' && value.includes('@');
 
-	const [enteredEmail, setEnteredEmail] = useState('');
-	const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+	const { value: enteredName,
+			isValueValid: isNameValid,
+			hasError: nameInputHasError,
+			valueChangeHandler: nameChangeHandler,
+			InputBlurHandler: nameBlurHandler,
+			reset: resetNameInput,
+	} = useInput(validateName);
 
-	const isEmailValid = () => {
-		if (enteredEmail.trim() === '') {
-			return false;
-		}
-		if (!enteredEmail.includes('@')) {
-			return false;
-		}
-		return true;
-	}
+	const { value: enteredEmail,
+		isValueValid: isEmailValid,
+		hasError: emailInputHasError,
+		valueChangeHandler: emailChangeHandler,
+		InputBlurHandler: emailBlurHandler,
+		reset: resetEmailInput,} = useInput(validateEmail);
 
-	const enteredNameIsValid = enteredName.trim() !== '';
-	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
-
-	const enteredEmailIsValid = isEmailValid();
-	const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
 	let formIsValid = false;
 
-	if (enteredNameIsValid && enteredEmailIsValid) {
+	if (isNameValid && isEmailValid) {
 		formIsValid = true;
-	}
-
-	const nameInputChangeHandler = (event) => {
-		setEnteredName(event.target.value);
-	};
-
-	const emailInputChangeHandler = (event) => {
-		setEnteredEmail(event.target.value);
-	};
-
-	const nameInputBlurHandler = (event) => {
-		setEnteredNameTouched(true);
-	}
-
-	const emailInputBlurHandler = (event) => {
-		setEnteredEmailTouched(true);
 	}
 
 	const formSubmissionHandler = (event) => {
 		event.preventDefault();
 
-		setEnteredNameTouched(true);
-		setEnteredEmailTouched(true);
-
-		if (!enteredNameIsValid || !enteredEmailIsValid) {
+		if (!isNameValid || !isEmailValid) {
 			return;
 		}
 
-		setEnteredName('');
-		setEnteredEmail('');
-		setEnteredNameTouched(false);
-		setEnteredEmailTouched(false);
+		resetNameInput();
+		resetEmailInput();
 	}
 
-	const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
-	const emailInputClasses = emailInputIsInvalid ? 'form-control invalid' : 'form-control';
+	const nameInputClasses = nameInputHasError ? 'form-control invalid' : 'form-control';
+	const emailInputClasses = emailInputHasError ? 'form-control invalid' : 'form-control';
 
 	return (
 	<form onSubmit={formSubmissionHandler}>
 		<div className={emailInputClasses}>
 			<label htmlFor='email'>Your E-mail</label>
-			<input type='email' id='email' onChange={emailInputChangeHandler} onBlur={emailInputBlurHandler} value={enteredEmail}/>
-			{emailInputIsInvalid && <p className="error-text">Email must not be empty and must contain @</p>}
+			<input type='email' id='email' onChange={emailChangeHandler} onBlur={emailBlurHandler} value={enteredEmail}/>
+			{emailInputHasError && <p className="error-text">Email must not be empty and must contain @</p>}
 		</div>
 		<div className={nameInputClasses}>
 			<label htmlFor='name'>Your Name</label>
-			<input type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName}/>
-			{nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
+			<input type='text' id='name' onChange={nameChangeHandler} onBlur={nameBlurHandler} value={enteredName}/>
+			{nameInputHasError && <p className="error-text">Name must not be empty</p>}
 		</div>
 		<div className="form-actions">
 			<button type="submit" disabled={!formIsValid}>Submit</button>
