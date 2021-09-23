7949f3edd0b93253a07461556b39cedef84d4b91
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-23 22:05:36 +0900

    Update: Add state for input

--- react-complete-guide/12-working-with-forms-user-input/src/components/SimpleInput.js
+++ react-complete-guide/12-working-with-forms-user-input/src/components/SimpleInput.js
@@ -1,15 +1,34 @@
+import { useState, useRef } from 'react'
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const [enteredName, setEnteredName] = useState('');
+	const nameInputRef = useRef();
+
+	const nameInputChangeHandler = (event) => {
+		setEnteredName(event.target.value);
+	};
+
+	const formSubmissionHandler = (event) => {
+		event.preventDefault();
+
+		console.log(enteredName);
+		const enteredValue = nameInputRef.current.value;
+		console.log(enteredValue);
+
+		setEnteredName('');
+	}
+
+	return (
+	<form onSubmit={formSubmissionHandler}>
+		<div className='form-control'>
+			<label htmlFor='name'>Your Name</label>
+			<input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName}/>
+		</div>
+		<div className="form-actions">
+			<button type="submit">Submit</button>
+		</div>
+	</form>
+	);
 };
 
 export default SimpleInput;
