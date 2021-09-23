9563283c863882031743d1d9508a2d6324e9ffff
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-23 12:52:33 +0900

    Update: Configure custom hook

--- react-complete-guide/10-create-custom-hooks/src/hooks/use-counter.js
+++ react-complete-guide/10-create-custom-hooks/src/hooks/use-counter.js
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (doForward = true) => {
 	const [counter, setCounter] = useState(0);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-		setCounter((prevCounter) => prevCounter + 1);
+		if (doForward) {
+			setCounter((prevCounter) => prevCounter + 1);
+		} else {
+			setCounter((prevCounter) => prevCounter - 1);
+		}
+
 	}, 1000);
 
 	return () => clearInterval(interval);
-	}, []);
+	}, [doForward]);
+
+	return counter;
 };
 
 export default useCounter;
