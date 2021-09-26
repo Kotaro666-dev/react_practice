86bbea2a482db504a79298e5fa6014f47dd6c93a
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-26 11:52:58 +0900

    Update: Add http request to get available meals

--- react-complete-guide/13-practice-http/src/components/Meals/AvailableMeals.js
+++ react-complete-guide/13-practice-http/src/components/Meals/AvailableMeals.js
@@ -1,52 +1,95 @@
+import React, { useEffect, useState, useCallback } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+const url = 'https://react-http-8a8f0-default-rtdb.firebaseio.com/meals.json';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState(null);
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch(url);
+			if (response.status !== 200) {
+				throw new Error('Something went wrong!');
+			}
+			const responseData = await response.json();
+
+			const meals = [];
+			for (const key in responseData) {
+				meals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+			setMeals(meals);
+		}
+
+		const fetchApi = async () => {
+			setIsLoading(true);
+			setErrorMessage(null);
+
+			try {
+				await fetchMeals();
+			} catch (error) {
+				setErrorMessage(error.message);
+			}
+			setIsLoading(false);
+		};
+
+		fetchApi();
+		return () => {};
+	}, []);
+
+
+	const mealsList = meals.map((meal) => (
+		<MealItem
+			key={meal.id}
+			id={meal.id}
+			name={meal.name}
+			description={meal.description}
+			price={meal.price}
+		/>
+	));
+
+	const getContent = () => {
+		if (isLoading) {
+			return (
+				<section className={classes.loading}>
+					<p>Loading...</p>
+				</section>
+			);
+		} else if (errorMessage) {
+			return (
+				<section className={classes['error-message']}>
+					<p>{errorMessage}</p>
+				</section>
+			);
+		} else if (mealsList.length > 0) {
+			return (
+				<section className={classes.meals}>
+					<Card>
+						<ul>{mealsList}</ul>
+					</Card>
+				</section>
+			);
+		}
+		return <p>Found no movies.</p>;
+	};
+
+	const content = getContent();
+
+	return (
+		<React.Fragment>
+			{content}
+		</React.Fragment>
+	);
 };
 
 export default AvailableMeals;
