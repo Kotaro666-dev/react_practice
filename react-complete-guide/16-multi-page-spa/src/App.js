5b2111ab9dc8426582570b437f01a9fe371eb2c0
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-29 08:36:39 +0900

    Update: Add pages and router

--- react-complete-guide/16-multi-page-spa/src/App.js
+++ react-complete-guide/16-multi-page-spa/src/App.js
@@ -1,9 +1,22 @@
+import { Route } from 'react-router-dom'
+
+import Welcome from './Pages/Welcome'
+import Products from './Pages/Products'
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<Route path="/welcome">
+				<Welcome />
+			</Route>
+			<Route path="/products">
+				<Products />
+			</Route>
+		</div>
+	);
 }
 
 export default App;
+
+// / => Component A
+// /products => Component B
