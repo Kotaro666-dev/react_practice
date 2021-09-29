fd7b275c8880541f083f922cee1c903f15ec3c09
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-30 08:30:22 +0900

    Update: Add Page Not Found component

---
+++ react-complete-guide/17-time-to-practice-starting-code/src/Pages/NotFound.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const NotFound = () => {
+	return (
+		<Fragment className="centered">
+			<p>Page Not Found!</p>
+		</Fragment>
+	);
+};
+
+export default NotFound;
