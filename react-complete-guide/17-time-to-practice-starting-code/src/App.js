a1f81c5c20f8fcdc007dfd6386eef2c21d880050
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-30 07:57:51 +0900

    Refactor: Remove unnecessary codes

--- react-complete-guide/17-time-to-practice-starting-code/src/App.js
+++ react-complete-guide/17-time-to-practice-starting-code/src/App.js
@@ -1,27 +1,25 @@
 import { Route, Switch, Redirect} from 'react-router-dom';
+
 import AllQuotes from './Pages/AllQuotes';
 import NewQuote from './Pages/NewQuote';
 import QuoteDetail from './Pages/QuoteDetail';
 
-
 function App() {
 	return (
-		<div>
-			<Switch>
-				<Route path="/" exact>
-					<Redirect to="/quotes"/>
-				</Route>
-				<Route path="/new-quote">
-					<NewQuote />
-				</Route>
-				<Route path="/quotes" exact>
-					<AllQuotes />
-				</Route>
-				<Route path="/quotes/:q1">
-					<QuoteDetail />
-				</Route>
-			</Switch>
-		</div>
+		<Switch>
+			<Route path="/" exact>
+				<Redirect to="/quotes"/>
+			</Route>
+			<Route path="/quotes" exact>
+				<AllQuotes />
+			</Route>
+			<Route path="/quotes/:quoteId">
+				<QuoteDetail />
+			</Route>
+			<Route path="/new-quote">
+				<NewQuote />
+			</Route>
+		</Switch>
 	);
 }
 
