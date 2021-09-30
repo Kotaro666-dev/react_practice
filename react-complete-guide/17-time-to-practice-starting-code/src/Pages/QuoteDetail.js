da9e85b57e15b992943e2bee901089b1bdb32918
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-30 09:09:43 +0900

    Update: Use nested route to implement load comments button

--- react-complete-guide/17-time-to-practice-starting-code/src/Pages/QuoteDetail.js
+++ react-complete-guide/17-time-to-practice-starting-code/src/Pages/QuoteDetail.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
-import { useParams, Route} from "react-router";
+import { useParams, Route } from "react-router";
+import { Link } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
@@ -43,6 +44,13 @@ const QuoteDetail = () => {
 	return (
 		<Fragment>
 			<HighlightedQuote author={quote.author} text={quote.text} />
+			<Route path={`/quotes/${params.quoteId}`} exact>
+				<div className="centered">
+					<Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+						Load Comments
+					</Link>
+				</div>
+			</Route>
 			<Route path={`/quotes/${params.quoteId}/comments`}>
 				<Comments />
 			</Route>
