24d7a1a7d10f6ccbd3ceda9b1860f49c3bc8ae70
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-03 13:16:49 +0900

    Update: use localStorage to keep loggedin

--- react-complete-guide/18-authentication/src/store/auth-slice.js
+++ react-complete-guide/18-authentication/src/store/auth-slice.js
@@ -1,22 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const localStorageKey = 'idToken';
+const initialIdTokenValue = localStorage.getItem(localStorageKey);
+
 const authSlice = createSlice({
 	name: 'auth',
 	initialState: {
-		idToken: '',
-		isLoggedIn: false,
+		idToken: initialIdTokenValue,
+		isLoggedIn: initialIdTokenValue ? true : false,
 	},
 	reducers: {
 		login(state, action) {
 			state.idToken = action.payload;
 			state.isLoggedIn = true;
+			localStorage.setItem(
+				localStorageKey, action.payload
+			);
 		},
 		logout(state) {
 			state.idToken = '';
 			state.isLoggedIn = false;
+			localStorage.removeItem(localStorageKey);
 		},
 		updateIdToken(state, action) {
+			localStorage.remove(localStorageKey);
 			state.idToken = action.payload;
+			localStorage.setItem(
+				localStorageKey, action.payload
+			);
 		}
 	}
 });
