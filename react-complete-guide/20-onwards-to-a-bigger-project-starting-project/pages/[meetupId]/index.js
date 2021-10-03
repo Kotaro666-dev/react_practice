e676fbe84f78fc1d9df8c82b98d95150cdc5701a
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-04 08:23:30 +0900

    Update: Add meetupDetail page

--- react-complete-guide/20-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
+++ react-complete-guide/20-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -1,5 +1,14 @@
-const MeetUpDetailPage = () => {
-  return <h1>Hello world!</h1>;
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetailPage = () => {
+  return (
+    <MeetupDetail
+      title="First meetup"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1024px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG"
+      address="Some address"
+      description="This is a first meetup"
+    />
+  );
 };
 
-export default MeetUpDetailPage;
+export default MeetupDetailPage;
