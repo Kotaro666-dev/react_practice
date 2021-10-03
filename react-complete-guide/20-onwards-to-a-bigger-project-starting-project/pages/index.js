cf17f2521043876dcdc193474eb37e6b930096a2
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-03 21:29:00 +0900

    Update: Add Dummy meetups

--- react-complete-guide/20-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ react-complete-guide/20-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,5 +1,27 @@
+import { useEffect } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1024px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG",
+    address: "Some address",
+    descripton: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1024px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG",
+    address: "Some address",
+    descripton: "This is a second meetup",
+  },
+];
+
 const HomePage = () => {
-  return <h1>Hello world!</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
