f3b66e9fe4d81071470e6676f407141048d952e9
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-04 22:53:14 +0900

    Update: Add insert data to database

--- react-complete-guide/20-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ react-complete-guide/20-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,9 +1,24 @@
-import {} from "react";
+import { useRouter } from "next/router";
+
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (meetupData) => {
-    console.log(meetupData);
+  const router = useRouter();
+
+  const addMeetupHandler = async (meetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(meetupData),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.replace("/");
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
