33187f05c740867580fc4360aa203895f67d2cd6
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-07 22:47:57 +0900

    Update: Use ref

--- react-complete-guide/22-react-ts-starting-project/src/components/NewTodo.tsx
+++ react-complete-guide/22-react-ts-starting-project/src/components/NewTodo.tsx
@@ -1,16 +1,22 @@
 import React, { useRef } from "react";
 
 const NewTodo = () => {
-  const todoTextInputRef = useRef();
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent): void => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label>Todo Text</label>
-      <input type="text" id="text" />
+      <input ref={todoTextInputRef} type="text" id="text" />
       <button type="submit">Add Todo</button>
     </form>
   );
