1ba2e1789b0a23077f41a42fa1037b90eabe1690
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-07 22:59:47 +0900

    Update: Add function

--- react-complete-guide/22-react-ts-starting-project/src/App.tsx
+++ react-complete-guide/22-react-ts-starting-project/src/App.tsx
@@ -1,12 +1,18 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todo";
 import Todo from "./models/todo";
 
-const todos = [new Todo("TEST1"), new Todo("TEST2")];
+const initialTodos = [new Todo("TEST1"), new Todo("TEST2")];
 
 function App() {
+  const addTodoHandler = (todoText: string) => {
+    console.log(todoText);
+  };
+
   return (
     <div>
-      <Todos items={todos} />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={initialTodos} />
     </div>
   );
 }
