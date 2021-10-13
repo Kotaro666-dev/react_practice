0c2017f691b0b7bf5f56937a424b5b597cf9339a
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-13 22:33:12 +0900

    Update: Add onChangeRaw property to get Zenkaku value

--- datepicker-practice/src/Components/example.tsx
+++ datepicker-practice/src/Components/example.tsx
@@ -6,13 +6,34 @@ import "react-datepicker/dist/react-datepicker.css";
 // CSS Modules, react-datepicker-cssmodules.css
 
 const Example = () => {
-    const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
 
-    return (
-        <div >
-            <DatePicker selected={startDate} onChange={(date: any) => setStartDate(date)} />
-        </div>
-    );
+  const onChangeHandler = (
+    value: Date | [Date | null, Date | null] | null,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    console.log(`onChangeHanlder.value = ${value}`);
+    console.log(`onChangeHanlder.event = ${event.target.value}`);
+  };
+
+  // This function can get 全角 value.
+  const onChangeRawHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`onChangeRawHandler.value = ${event.target.value}`);
+  };
+
+  return (
+    <div>
+      <DatePicker
+        selected={startDate}
+        onChangeRaw={(event: React.ChangeEvent<HTMLInputElement>) =>
+          onChangeRawHandler(event)
+        }
+        onChange={(value, event: React.ChangeEvent<HTMLInputElement>) =>
+          onChangeHandler(value, event)
+        }
+      />
+    </div>
+  );
 };
 
 export default Example;
