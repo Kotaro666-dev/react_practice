e7b9946de833aee40fa59a0322251eec29c70770
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-14 09:00:30 +0900

    Update: Add several tests

--- react-complete-guide/23-unit-test/src/components/Greeting.test.js
+++ react-complete-guide/23-unit-test/src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Greeting from "./Greeting";
 
@@ -13,5 +14,36 @@ describe("Greeting components", () => {
     // Assert
     const element = screen.getByText("Hello World", { exact: false });
     expect(element).toBeInTheDocument();
+  });
+
+  test("Renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const element = screen.getByText(`good to see you`, { exact: false });
+    expect(element).toBeInTheDocument();
+  });
+
+  test("Renders Changed here if the button was clicked", () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const element = screen.getByText(`Changed here`, { exact: false });
+    expect(element).toBeInTheDocument();
+  });
+
+  test("Do not render good to see you if the button was clicked", () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const element = screen.queryByText(`good to see you`, { exact: false });
+    expect(element).toBeNull();
   });
 });
