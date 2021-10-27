72d3487fb8a7ea4cab77762b015cca95f7a42731
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-27 09:41:05 +0900

    Update: Add dispatch

--- todo_app/src/components/AddTodo.tsx
+++ todo_app/src/components/AddTodo.tsx
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
 
+import {useAppDispatch, Todo} from '../store/store';
+import {addTodo} from '../store/todo-reducer';
+
 const AddTodo = () => {
   const [newTodoInput, setNewTodoInput] = useState<string>('');
+  const dispatch = useAppDispatch();
 
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const text = event.target.value;
@@ -9,10 +13,16 @@ const AddTodo = () => {
     setNewTodoInput(text);
   };
 
-  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitHandler = (event: React.SyntheticEvent) => {
     event.preventDefault();
 
     // Add state
+    const newTodo: Todo = {
+      id: 1,
+      title: newTodoInput,
+      isDone: false,
+    };
+    dispatch(addTodo(newTodo));
 
     setNewTodoInput('');
   };
