c9c3706064b505ebb6dc78fda0b69cfb17429dbd
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-27 09:40:29 +0900

    Update: replace dummy todos with todos from store

--- todo_app/src/components/TodoList.tsx
+++ todo_app/src/components/TodoList.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
 import TodoItem from './TodoItem';
+import {useSelector} from '../store/store';
 
-const DUMMY_TODOS = [
-  {id: 1, title: 'Test1', isDone: false},
-  {id: 2, title: 'Test2', isDone: false},
-  {id: 3, title: 'Test3', isDone: false},
-];
+// const DUMMY_TODOS = [
+//   {id: 1, title: 'Test1', isDone: false},
+//   {id: 2, title: 'Test2', isDone: false},
+//   {id: 3, title: 'Test3', isDone: false},
+// ];
 
 const TodoList = () => {
+  const todos = useSelector(state => state.todo);
+
   return (
     <ul>
-      {DUMMY_TODOS.map(item => (
+      {todos.map(item => (
         <TodoItem id={item.id} title={item.title} isDone={item.isDone} />
       ))}
     </ul>
