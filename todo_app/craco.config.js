245bb3813a5c8793c12e5825a5220b3360ff7b55
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-28 09:04:23 +0900

    Update: Disable eslint

--- todo_app/craco.config.js
+++ todo_app/craco.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   style: {
     postcss: {
+      // eslint-disable-next-line node/no-unpublished-require
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
